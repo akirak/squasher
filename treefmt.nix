2eb19bc15d2788630470d19ab20d10c70af68180
Akira Komamura <akira.komamura@gmail.com>
2026-05-24 22:12:33 +0900

    style: Update the formatter configuration

--- treefmt.nix
+++ treefmt.nix
@@ -1,16 +1,9 @@
 {
   projectRootFile = "treefmt.nix";
 
-  # See https://github.com/numtide/treefmt-nix#supported-programs
-
   programs.gofmt.enable = true;
 
-  # GitHub Actions
-  programs.yamlfmt.enable = true;
-  programs.actionlint.enable = true;
-
-  # Markdown
-  programs.mdformat.enable = true;
+  programs.zizmor.enable = true;
 
   # Nix
   programs.nixfmt.enable = true;
